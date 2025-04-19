eafc0f59f850a6639a681cfdd85b5e193b298c83
imdavyking <yungdynamic53@gmail.com>
2025-04-19 20:50:45 +0100

    chore: updated set

--- backend/src/routes/fdc.routes.ts
+++ backend/src/routes/fdc.routes.ts
@@ -3,5 +3,5 @@ import { getJsonProof, getSample } from "../controllers/fdc.controllers";
 
 const fdcRoutes = express.Router();
 fdcRoutes.post("/json-proof", getJsonProof);
-fdcRoutes.post("/sample-open-weather", getSample);
+fdcRoutes.get("/sample-open-weather", getSample);
 export default fdcRoutes;
