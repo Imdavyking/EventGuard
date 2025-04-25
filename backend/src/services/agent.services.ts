615f656af157240daa84a8c5814e877e1c2fed9d
imdavyking <yungdynamic53@gmail.com>
2025-04-25 19:36:02 +0100

    chore: fix chat history

--- backend/src/services/agent.services.ts
+++ backend/src/services/agent.services.ts
@@ -1,14 +1,21 @@
 import { ChatOpenAI } from "@langchain/openai";
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import {
+  BaseMessage,
+  HumanMessage,
+  SystemMessage,
+  ToolMessage,
+} from "@langchain/core/messages";
 import dotenv from "dotenv";
 import { environment } from "../utils/config";
 import { tools } from "../utils/agent.tools";
+
 dotenv.config();
 const openAIApiKey = environment.OPENAI_API_KEY!;
 const llm = new ChatOpenAI({
   model: "gpt-4o-mini",
   apiKey: openAIApiKey,
 });
+const chatHistory: Record<string, BaseMessage[]> = {};
 
 export async function runAIAgent({
   userPrompt,
@@ -23,13 +30,30 @@ export async function runAIAgent({
   const llmWithTools = llm.bind({
     tools,
   });
-  const result = await llmWithTools.invoke([
-    systemPrompt,
-    new HumanMessage(userPrompt),
-  ]);
+  const userMessage = new HumanMessage(userPrompt);
+  if (typeof chatHistory[userAddress] === "undefined") {
+    chatHistory[userAddress] = [];
+  }
+  chatHistory[userAddress].push(userMessage);
+  const messages = [systemPrompt, ...chatHistory[userAddress], userMessage];
+
+  // Invoke the AI agent
+  const aiMessage = await llmWithTools.invoke(messages);
+
+  chatHistory[userAddress].push(aiMessage);
+
+  if (aiMessage.tool_calls && aiMessage.tool_calls.length > 0) {
+    aiMessage.tool_calls.forEach((toolCall) => {
+      const message = new ToolMessage({
+        content: toolCall.name,
+        tool_call_id: toolCall.id!,
+      });
+      chatHistory[userAddress].push(message);
+    });
+  }
 
   return {
-    content: result.content,
-    tool_calls: result.tool_calls,
+    content: aiMessage.content,
+    tool_calls: aiMessage.tool_calls,
   };
 }
