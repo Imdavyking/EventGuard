7592828072e0996b0e7dc5559e9804d680e69b7e
imdavyking <yungdynamic53@gmail.com>
2025-04-25 12:10:51 +0100

    chore: updated

--- backend/src/services/agent.services.ts
+++ backend/src/services/agent.services.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import { environment } from "../utils/config";
 import { MemorySaver } from "@langchain/langgraph";
 import { tools } from "../utils/agent.tools";
+
+// ConversationBufferMemory learn about this
 dotenv.config();
 const openAIApiKey = environment.OPENAI_API_KEY!;
 const checkpointSaver = new MemorySaver();
@@ -11,6 +13,7 @@ const llm = new ChatOpenAI({
   model: "gpt-4o-mini",
   apiKey: openAIApiKey,
 });
+
 export async function runAIAgent({
   userPrompt,
   userAddress,
